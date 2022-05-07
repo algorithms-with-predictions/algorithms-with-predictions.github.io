1766a35fb143e5bf803a2538617468a6b5ee2bb5
Alexander Lindermayr <linderal@uni-bremen.de>
2022-05-07 19:11:25 +0200

    open markdown links in new tab

--- gatsby-config.js
+++ gatsby-config.js
@@ -17,6 +17,13 @@ module.exports = {
       resolve: 'gatsby-transformer-remark',
       options: {
         plugins: [
+          {
+            resolve: "gatsby-remark-external-links",
+            options: {
+              target: "_blank",
+              rel: "noopener noreferrer"
+            }
+          },
           `gatsby-remark-prismjs-copy-button`,
           {
             resolve: `gatsby-remark-prismjs`,
