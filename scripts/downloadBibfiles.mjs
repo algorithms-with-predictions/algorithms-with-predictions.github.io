4ae7f16848236a7c0c71eb7f6a8bd0b6c5b7f858
Alexander Lindermayr <linderal@uni-bremen.de>
2022-05-15 18:08:09 +0200

    wip

--- scripts/downloadBibfiles.mjs
+++ scripts/downloadBibfiles.mjs
@@ -5,48 +5,56 @@ import { XMLParser } from "fast-xml-parser";
 import fastls from "fast-levenshtein";
 
 const paper_dir = "papers";
-const papers = fs.readdirSync(paper_dir);
-console.log(papers);
+const papers = fs.readdirSync(paper_dir);//.slice(0, 10);
 
-await Promise.all(
+let updated = await Promise.all(
   papers.map(async (file) => {
     let paper = yaml.load(
       fs.readFileSync(paper_dir + "/" + file, { encoding: "utf-8" })
     );
+    //console.log(paper.title);
 
-    let info = await axios.get(
-      "https://dblp.org/search/publ/api?q=" +
-        paper.title
-          .split(" ")
-          .map((t) => t + "$")
-          .join("+")
-    );
+    try {
+      let info = await axios.get(
+        "https://dblp.org/search/publ/api?q=" +
+          paper.title.split(" ").join("+"),
+        { timeout: 10000 }
+      );
 
-    const data = info.data;
-    const parser = new XMLParser();
-    let dataObj = parser.parse(data);
-    let hits = [dataObj.result.hits.hit].flat();
-    console.log(hits);
+      let data = info.data;
+      let parser = new XMLParser();
+      let dataObj = parser.parse(data);
+      let hits = [dataObj.result.hits.hit].flat();
+      //console.log(paper.title);
 
-    hits.forEach((hit) => {
-      console.log(hit);
-      if (hit === undefined) {
-        return;
-      }
-
-      let title = hit.info.title;
-      if (fastls.get(title, paper.title) < 10) {
-        const venue = hit.info.venue === "CoRR" ? "arXiv" : hit.info.venue;
-        if (!paper.publications.some((pub) => pub.name === venue)) {
-          paper.publications.push({
-            name: venue,
-            year: hit.info.year,
-            url: hit.info.ee,
-          });
+      hits.forEach((hit) => {
+        //console.log(hit);
+        if (hit === undefined) {
+          return;
         }
-      }
-    });
 
-    fs.writeFileSync("papers/" + file + ".yml", yaml.dump(paper));
+        let title = hit.info.title;
+        if (fastls.get(title, paper.title) < 10) {
+          const venue = hit.info.venue === "CoRR" ? "arXiv" : hit.info.venue;
+          if (!paper.publications.some((pub) => pub.name === venue)) {
+            paper.publications.push({
+              name: venue,
+              year: hit.info.year,
+              url: hit.info.ee,
+            });
+          }
+        }
+      });
+      return [file, paper];
+    } catch (error) {
+      console.log(error);
+      return [file, paper];
+    }
   })
+);
+
+console.log(updated)
+
+updated.forEach(([file, paper]) =>
+  fs.writeFileSync("papers/" + file, yaml.dump(paper))
 );
